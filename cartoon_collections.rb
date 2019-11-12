46d843a5b9a2be213dbd48b55ee450ce41a065c5
Baruch Phillips <baruchphillips@gmail.com>
2019-11-12 22:34:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(source)
   source.any? { |index| index.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include
 end
