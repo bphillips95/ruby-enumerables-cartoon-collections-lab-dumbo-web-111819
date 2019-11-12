cdaf3cfccb372b9d21952d67ecfd524abcaa7744
Baruch Phillips <baruchphillips@gmail.com>
2019-11-12 22:30:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
+  planeteer_calls.any? { |index| index.length > 4 }
 end
 
 def find_the_cheese# code an argument here
