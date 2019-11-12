3dfc40d76a9a99fefc65a46c7915b4d8227e8af0
Baruch Phillips <baruchphillips@gmail.com>
2019-11-12 22:08:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
   array.each_with_index do |array,index| 
-  puts "#{array"
-  }
+  puts "#{index}. #{array}"
+end 
 
 
 def summon_captain_planet# code an argument here
